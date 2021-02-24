aedceff46f81f6d5504685ac682076ee53df887e
pyoLeo <pyo8880@gmail.com>
2021-02-24 15:53:27 +0900

    Revert "master #4"
    This reverts commit 666c4c853a385c33a845cbf458170331cd77a42a.

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//#4
 //#3
 //#2
 @Controller
