b00e95048fd36c054daed1634a59fba5038b88c3
pyoLeo <pyo8880@gmail.com>
2021-02-24 15:53:32 +0900

    Revert "master #3"
    This reverts commit 15b7141b5396184e1df64d9f3a6732f7af4f088a.

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//#3
 //#2
 @Controller
 public class HomeController {
