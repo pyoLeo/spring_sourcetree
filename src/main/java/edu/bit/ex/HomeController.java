b88f6d07b673234d1797889c1483a98ec42047dd
pyoLeo <pyo8880@gmail.com>
2021-02-24 15:49:45 +0900

    master #5

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,8 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+<<<<<<< HEAD
 //#4
 //#3
+=======
+>>>>>>> parent of 15b7141 (master #3)
 //#2
 @Controller
 public class HomeController {
