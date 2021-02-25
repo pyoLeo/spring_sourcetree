3c8eb88d019037f007a9db08fba8c3977997cc25
pyoLeo <pyo8880@gmail.com>
2021-02-25 11:18:36 +0900

    develop #3

--- src/main/java/edu/bit/ex/BoardController.java
+++ src/main/java/edu/bit/ex/BoardController.java
@@ -11,7 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//#3 board
+//#3 board develop
 //#2 add test
 @Controller
 public class BoardController {
